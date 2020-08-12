dccdd16c7882839b5c9060757c40734ceed3f3fb
Amanda <amanda.asilva@hotmail.com.br>
2020-08-12 06:22:02 -0300

    SellerFormViewModel and Department select component

--- Sales_Web_MVC/Controllers/SellersController.cs
+++ Sales_Web_MVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Sales_Web_MVC.Models;
+using Sales_Web_MVC.Models.ViewModels;
 using Sales_Web_MVC.Services;
 
 namespace Sales_Web_MVC.Controllers
@@ -12,10 +13,12 @@ namespace Sales_Web_MVC.Controllers
     {
 
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -26,7 +29,9 @@ namespace Sales_Web_MVC.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
--- Sales_Web_MVC/Services/DepartmentService.cs
+++ Sales_Web_MVC/Services/DepartmentService.cs
@@ -7,25 +7,19 @@ using System.Threading.Tasks;
 
 namespace Sales_Web_MVC.Services
 {
-    public class SellerService 
+    public class DepartmentService
     {
         private readonly Sales_Web_MVCContext _context;
 
-        public SellerService(Sales_Web_MVCContext context)
+        public DepartmentService(Sales_Web_MVCContext context)
         {
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public List<Department> FindAll()
         {
-            return _context.Seller.ToList();
+            return _context.Department.OrderBy(x => x.Name).ToList();
         }
 
-        public void Insert(Seller obj)
-        {
-            
-            _context.Add(obj);
-            _context.SaveChanges();
-        }
     }
 }
